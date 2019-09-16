1567eee12d9a1e1710faa82acfdfc75117843e5b
ManiNagarajan20 <Odata2?$=GET>
2019-09-16 17:11:19 -0500

    2,3 exc completed

--- cloudmesh-exercises/e-cloudmesh-common-2.py
+++ cloudmesh-exercises/e-cloudmesh-common-2.py
@@ -1,2 +1,17 @@
 # fa19-516-156 E.Cloudmesh.Common.2
 # Develop a program that demonstartes the use of dotdict.
+
+from cloudmesh.common.dotdict import dotdict
+data = {
+"Book": "Azure"
+}
+data = dotdict(data)
+var = data.Book
+print(var)
+
+#Use Logical condition if..else..
+if data.Book is "Azure":
+    print("Cloud Engineering using Azure")
+else:
+    print("Other Cloud Books")
+
--- cloudmesh-exercises/e-cloudmesh-common-3.py
+++ cloudmesh-exercises/e-cloudmesh-common-3.py
@@ -1,2 +1,23 @@
 # fa19-516-156 E.Cloudmesh.Common.3
 # Develop a program that demonstartes the use of FlatDict.
+
+from cloudmesh.common.FlatDict import FlatDict
+import flatdict
+
+values = {'City': {'Houston': {'Offices': 4,
+                               'Cars': 20,
+                               'Employees': 25},
+                   'Newyork': {'Offices': 10,
+                               'Cars': 25,
+                               'Employees': 45}
+                   }
+          }
+
+flat = flatdict.FlatDict(values)
+
+## Print all city details flatdict
+print(flat['City'])
+
+## Print each city details.
+print(flat['City:Houston'])
+print(flat['City:Newyork'])
