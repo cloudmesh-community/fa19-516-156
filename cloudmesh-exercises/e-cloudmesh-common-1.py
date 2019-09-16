3654c16d389c9292cd131f9cbe9bd76362c2b521
ManiNagarajan20 <Odata2?$=GET>
2019-09-16 12:24:17 -0500

    First exercise success test

--- cloudmesh-exercises/e-cloudmesh-common-1.py
+++ cloudmesh-exercises/e-cloudmesh-common-1.py
@@ -1,2 +1,25 @@
 # fa19-516-156 E.Cloudmesh.Common.1
 # Objective: Develop a program that demonstartes the use of banner, HEADING, and VERBOSE
+from cloudmesh.common.console import Console
+from cloudmesh.common.util import banner
+from cloudmesh.common.util import HEADING
+from cloudmesh.common.debug import VERBOSE
+
+banner("Cloud Programming")
+
+mesg = "console test"
+
+# test Console
+Console.ok(mesg)
+Console.msg(mesg)
+
+
+class Example(object):
+
+    def doit(self):
+        HEADING()
+        Print(" Heading")
+
+
+m = {"key": "value"}
+VERBOSE(m)
