2f7eb25f4cb0efd932ab2674f4b9bb99d7fe7255
ManiNagarajan20 <Odata2?$=GET>
2019-09-16 20:19:43 -0500

    4 ex completed

--- cloudmesh-exercises/e-cloudmesh-common-4.py
+++ cloudmesh-exercises/e-cloudmesh-common-4.py
@@ -1,2 +1,20 @@
 # fa19-516-156 E.Cloudmesh.Common.4
 # Develop a program that demonstartes the use of cloudmesh.common.Shell.
+
+from cloudmesh.common.Shell import Shell
+
+##Check Python Version
+result = Shell.check_python()
+print(result)
+
+## Check Valid Shell command
+result = Shell.command_exists("print")
+print(result)
+
+result = Shell.command_exists("output")
+print(result)
+
+
+result = Shell.terminal_type()
+print(result)
+
